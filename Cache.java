aa95868d6be8aea0925137b999f9fc0b42473165
Jorge Pereira <jff.pereira@campus.fct.unl.pt>
2018-11-30 14:57:39 +0000

    instable4

--- Cache.java
+++ Cache.java
@@ -9,50 +9,8 @@ import java.net.URL;
 import java.nio.file.Files;
 
 public class Cache {
-	public static final int CACHE_SIZE = 4;
-	public static final int REQUEST_SIZE = 50000;
 	
 	
-	private void removeOldestFile(File[] directoryListing) {
-		  File oldestFile = null;
-		  
-		  
-		    for (File child : directoryListing) {
-		    		if(oldestFile == null || oldestFile.lastModified() > child.lastModified())
-		    			oldestFile = child;
-		    }
-		    
-		    if(oldestFile != null) {
-		    	oldestFile.delete();
-		    }
-	}
 	
-	private void cleanCache() {
-		File dir = new File(Stream.SERVER_FILES);
-		File[] directoryListing = dir.listFiles();
-		
-		if(CACHE_SIZE <= directoryListing.length)
-			removeOldestFile(directoryListing);
-		
-		
-	}
 	
-	public void requestFileToServer(String server,String fileName) throws Exception {
-		BufferedInputStream in = new BufferedInputStream(new URL(server + "/" + fileName).openStream());
-		FileOutputStream out = new FileOutputStream(Stream.SERVER_FILES  + fileName);
-		
-		cleanCache();
-		
-		int bytesRead = -1;
-		byte[] buffer = new byte[REQUEST_SIZE];
-		
-		while((bytesRead = in.read(buffer,0,REQUEST_SIZE)) != -1) {
-			out.write(buffer, 0, bytesRead);
-		}
-			
-		in.close();
-		out.close();
-			
-	}
-
 }
--- CacheTask.java
+++ CacheTask.java
@@ -1,20 +1,65 @@
-class CacheTask extends Thread{
-	   private  Cache c;
-	   private  String fileName;
-	   private String contentServerURLPrefix;
-	   
-	   public CacheTask(String fileName,String contentServerURLPrefix) {
-		   c = new Cache();
-		   this.fileName = fileName;
-		   this.contentServerURLPrefix = contentServerURLPrefix;
-	   }
-	   
-	   public void run() {
-		   try {
-			c.requestFileToServer(contentServerURLPrefix,fileName);
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URL;
+
+class CacheTask extends Thread {
+	public static final int CACHE_SIZE = 4;
+	public static final int REQUEST_SIZE = 50000;
+	private String fileName;
+	private String contentServerURLPrefix;
+
+	private void removeOldestFile(File[] directoryListing) {
+		File oldestFile = null;
+
+		for (File child : directoryListing) {
+			if (oldestFile == null || oldestFile.lastModified() > child.lastModified())
+				oldestFile = child;
+		}
+
+		if (oldestFile != null) {
+			oldestFile.delete();
+		}
+	}
+
+	private void cleanCache() {
+		File dir = new File(Stream.SERVER_FILES);
+		File[] directoryListing = dir.listFiles();
+
+		if (CACHE_SIZE <= directoryListing.length)
+			removeOldestFile(directoryListing);
+
+	}
+
+	public void requestFileToServer(String server, String fileName) throws Exception {
+		BufferedInputStream in = new BufferedInputStream(new URL(server + "/" + fileName).openStream());
+		FileOutputStream out = new FileOutputStream(Stream.SERVER_FILES + fileName);
+
+		cleanCache();
+
+		int bytesRead = -1;
+		byte[] buffer = new byte[REQUEST_SIZE];
+
+		while ((bytesRead = in.read(buffer, 0, REQUEST_SIZE)) != -1) {
+			out.write(buffer, 0, bytesRead);
+		}
+
+		in.close();
+		out.close();
+
+	}
+
+	public CacheTask(String fileName, String contentServerURLPrefix) {
+		this.fileName = fileName;
+		this.contentServerURLPrefix = contentServerURLPrefix;
+	}
+
+	public void run() {
+		try {
+			requestFileToServer(contentServerURLPrefix, fileName);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		   
-	   }
-   }
+
+	}
+}
